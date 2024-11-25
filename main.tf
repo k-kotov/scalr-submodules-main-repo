9bcf1a164e751866c6d2029a0f6114c835331d52
penja <a.penchuk@scalr.com>
2024-11-25 13:07:29 +0200

    Add submodules

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "local" {}
+
+resource "null_resource" "networking_submodule" {
+  # Trigger the null_resource if the submodule is missing or if .gitmodules changes
+  triggers = {
+    gitmodules_hash = filemd5(".gitmodules")
+    submodule_exists = fileexists("modules/example-module/.git")
+  }
+}
+
+# Use the submodule as a Terraform module
+module "network" {
+  depends_on = [null_resource.networking_submodule] # Ensure the submodule is initialized first
+  source     = "./modules/example-module"
+  name = var.name
+}
+
+output "vpc_id" {
+  value = module.network.uuid
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "name" {
+  description = "The name to echo"
+  type        = string
+}
